b9d8ec0241db61e154e581bfd062402c39d25b23
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-30 18:47:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    # erb :say_words
+    # "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    erb :say_words
   end
 
   get '/:operation/:number1/:number2' do
