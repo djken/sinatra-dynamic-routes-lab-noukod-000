8b4b52b90c49b8099e6cc7020ab856479004a504
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-30 18:41:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,16 +19,5 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @num1, @num2, @operation = params[:number1].to_i, params[:number2].to_i, params[:operation]
-    case @operation
-    when 'add'
-      "#{@num1 + @num2}"
-    when 'subtract'
-      "#{@num1 - @num2}"
-    when 'multiply'
-    "#{@num1 * @num2}"
-    when 'divide'
-    "#{@num1 / @num2}"
-    end
-  end
+    erb :operation
 end
